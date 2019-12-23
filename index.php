7301fd34f3d1e4a833550bcca66a7456460da33d
crack-id <58321747+crack-id@users.noreply.github.com>
2019-12-23 19:10:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<title>Hacked MR. CR4CK_ID</title>>
+  <script>alert ('Ngapain Om') </script>
+<script>alert ('Patch Your Sistem')  </script>
+   
+   <meta charset="utf-8">
+   <meta content="CR4CK" name="description"/>
+   <meta content="CR4CK" name="keyword"/>
+  <style type="text/css">
+			body { 
+
+				
+
+				background-image:url('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRJ0cTITvkiL_sL8h880umXliSLnVneAJL50CJogbWHRK8N_cr2');
+				background-color: #000000;
+				background-repeat:no-repeat;
+				background-size: 100% ;
+				background-position:top center;
+				
+			}
+			
+		</style
+     </head>
+         &nbsp;     
+           <center><img src=""width="300"height="300"></center></body>
+           
+           
+
+            
+             <center><font face="Forte" size="9" style="color:#ffffff;text-shadow:#FF0099 0px 0px 10px">HACKED BY MR. CR4CK_ID</font></center>
+               
+               <center><font style="color:#ffffff;text-shadow: 0px 0px 10px blue" size="7">.... :: CR4CK :: ....</font></center>
+               
+           <center><font color="yellow" size="6">- - - - - - - - - - - - - - - - -  </font></center>
+            
+             <center><font face="Stencil" size="5" color="red">[ Hello Admin,<font face="Stencil" size="5" color="green"> YOUR WEBSITE IS NOT SAFE!! TOLONG BENERIN WEBSITE MU ORANG JAHAT BERAWAL DARI ORANG BAIK YANG TERSAKITI I WELL COMEBACK EROR 404 BY MR. CR4CK INDONESIAN CYBER  ]<br>
+             <font style="color:red;text-shadow:#000 0px 0px 3px">KEEP <font style="color:white;text-shadow:#000 0px 0px 3px">SMILE ]<br>
+             	<center><font color="yellow" size="6">- - - - - - - - - - - - - - - - - </font></center>
+             <center><font face="Forte" size="9" style="color:#ffffff;text-shadow:#FF0099 0px 0px 10px">THANKS TO:</font></center>
+             <center><font color="yellow" size="6">- - - - - - - - - - - - - - - - - </font></center>
+         <div class="greets">
+<table align=center border="0">
+<tr>
+<td width=100% id=greetz>
+<marquee behavior="scroll" direction="left" scrollamount="4" scrolldelay="55" width="100%">
+<font size="5px" style="font-family: Iceland, sans-serif;color:white;text-shadow: 0 0 3px red, 0px 0px 5px red" >
+<b>-=|[MR.SPONGEBOB] [ GARYY D.F.K]  [./X.C X8Host]  [SPONGEBOB] [ .Silent] [ Mrs.QuenToxic] [ CHMODv1] [ RIZAL_ZXX] [ ./NANDA] [ ./ARYA] [./DIKI] [ MR.007 ]  [MR.SPONGEBOB] [TN.1NJ3CTOR ] [ BENJAMIN ] [P14nk70n] [ CYNAX ] [MINE 17] [RIjall404] [TN T17] [MR. CR4CK_ID]|=-</font>
+</marquee>
+</td>
+</table></div> 
+            <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/daun-berguguran.js" type="text/javascript"></script><script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju-2.js" type="text/javascript"></script> 
+          <center><font color="yellow" size="6">- - - - - - - - - - - - - - - - - </font></center>
+          
+<div><iframe width="300" height="50" src="https://vocaroo.com/embed/7eTzoP7nhOb" frameborder="0"></iframe><br><a href="https://voca.ro/7eTzoP7nhOb"
+
